d1e6de599886d100539f1fc666a40243d7b13e13
LightBen <4364056+LightBen@users.noreply.github.com>
2026-02-24 21:15:43 +0200

    feat: Establish initial multi-locale website structure, content pages, components, and assets.

--- legacy/src/utils/api.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-
-export const getMainNav = () =>
-  Vue.flamelinkApp.nav.getItems("mainNavigation", { structure: "nested" });
--- public/sw.js
+++ public/sw.js
@@ -4,9 +4,7 @@ const ASSETS_TO_CACHE = [
     '/index.html',
     '/manifest.json',
     '/cm-logo-icon.svg',
-    '/favicon.ico',
-    '/img/constantine-jlm-sm.png',
-    '/img/constantine-jlm.png'
+    '/favicon.ico'
 ];
 
 // Install Event
